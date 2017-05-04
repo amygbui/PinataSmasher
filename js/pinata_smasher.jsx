7374537e8fb4b3ba81e9f8e5b7f2f5e48628acf0
Amy Bui <amygbui@gmail.com>
2017-05-03 21:18:45 -0400

    Get pinata to move across screen

--- js/pinata_smasher.jsx
+++ js/pinata_smasher.jsx
@@ -4,22 +4,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const canvas = document.getElementById('pinata');
   const stage = new createjs.Stage(canvas);
 
-  const text = new createjs.Text("Piñata Smasher", "36px Rock Salt", "#000");
-  text.x = 275;
-  text.y = 100;
-
   const pinata = new Pinata().random();
   // debugger
   pinata.x = 100;
   const yPos = Math.round(Math.random() * canvas.height);
   console.log(yPos)
   pinata.y = yPos;
-
-
   stage.addChild(pinata);
-  stage.addChild(text);
 
-  stage.update();
+  createjs.Ticker.addEventListener("tick", tick);
+  createjs.Ticker.setFPS(40);
+  
+  function tick(event) {
+    pinata.x = pinata.x + 3;
+    // if (pinata.x > stage.canvas.width) { pinata.x = 0; }
+    stage.update(event);
+  }
 
-  createjs.Ticker.on("tick", stage);
+  // createjs.Ticker.on("tick", stage);
 });
