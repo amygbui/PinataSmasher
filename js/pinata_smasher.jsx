08a51a980ddc3c28df2a7f3396b105fe8e78bb41
Amy Bui <amygbui@gmail.com>
2017-05-06 22:16:20 -0400

    Fix restart text and game score

--- js/pinata_smasher.jsx
+++ js/pinata_smasher.jsx
@@ -17,21 +17,24 @@ document.addEventListener('DOMContentLoaded', () => {
 	hit.graphics.beginFill("#000").drawRect(-300, -300, canvas.width, canvas.height);
 	start.hitArea = hit;
 
+  const restart = new createjs.Text("", "bold 25px Gloria Hallelujah", "#000000");
+
   start.addEventListener("click", (e) => {
     stage.removeChild(start);
+    stage.removeChild(restart);
     score.score = 0;
     score.scoreText.text = `Score: ${score.score}`;
+
+    restart.text = "(Click anywhere to restart)"
+    restart.x = resize(restart);
+    restart.y = start.y + 50;
+
     game.start();
     stage.update(event);
 
     setTimeout(() => {
       start.text = `Game over! Your score was ${score.score}`
       start.x = resize(start);
-
-      const restart = new createjs.Text("(Click anywhere to restart)", "bold 25px Gloria Hallelujah", "#000000");
-      restart.x = resize(restart);
-      restart.y = start.y + 50;
-
       game.end();
       stage.addChild(score.scoreText);
       stage.addChild(start);
@@ -42,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
   start.x = resize(start);
   start.y = 350;
   stage.addChild(start);
-
+  stage.addChild(restart);
   stage.update();
 });
 
